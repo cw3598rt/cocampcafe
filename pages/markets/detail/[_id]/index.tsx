7e6bc19935e538241828986139175d2b7fab6f32
김지수 <kimjisu@gimjisuui-MacBookAir.local>
2022-08-22 18:44:11 +0900

    [추가]feature#35 마켓 상세페이지 기본 틀 작성

---
+++ pages/markets/detail/[_id]/index.tsx
@@ -0,0 +1,5 @@
+import MarketDetail from "../../../../src/components/units/markets/detail/marketDetail.container";
+
+export default function MarketDetailPage() {
+  return <MarketDetail />;
+}
---
+++ src/components/units/markets/detail/marketDetail.presenter.tsx
@@ -0,0 +1,7 @@
+export default function MarketDetailUI(props) {
+  return (
+    <section>
+      <h1>{props.data?.fetchUseditem.name}</h1>
+    </section>
+  );
+}
